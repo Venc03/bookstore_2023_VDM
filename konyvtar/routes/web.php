751cafa64aacf83ccbc5248fcf32bd146dc3e159
Venc03 <vegi.dani1@gmail.com>
2023-09-26 12:00:10 +0200

    Orai munka

--- konyvtar/routes/web.php
+++ konyvtar/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\KonyvtaController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/api/books', [KonyvtaController::class, 'index']);
+Route::get('/api/books/{id}', [KonyvtaController::class, 'show']);
+Route::post('/api/books', [KonyvtaController::class, 'store']);
+Route::put('/api/books/{id}', [KonyvtaController::class, 'update']);
+Route::delete('/api/books/{id}', [KonyvtaController::class, 'destroy']);
+
+/* View utvonal */
+
+Route::get('/book/list', [KonyvtaController::class, 'listView']);
+Route::get('/book/edit', [KonyvtaController::class, 'editView']);
+Route::get('/book/view', [KonyvtaController::class, 'newView']);
